f93840a9ee3d4b37d110fc8931c814835a8ff00d
Amit Chauhan <158015945+amitcoderhub@users.noreply.github.com>
2024-12-11 19:47:17 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "Full Stack Web Development"],
+    strings: ["frontend development", "backend development", "web designing", "UI/UX designer","Python Developer", "Full Stack Web Development"],
     loop: true,
     typeSpeed: 60,
     backSpeed: 30,
